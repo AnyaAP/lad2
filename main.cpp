dea64adc1c8525178cd963dc8dd3fd1683096ec2
AnyaAP <91624798+AnyaAP@users.noreply.github.com>
2022-06-03 12:20:44 +0300

    Added comments

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
    #include <string>
 
    int main(){
-    std::string name;
-    std::cin >> name;
+    string name; \\ name
+    std::cin >> name; \\ input
     std::cout << "Hello world from " << name << std::endl;
    }
